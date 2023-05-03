af597c112b3cb3a0458129a4b5c54341aedc1507
Omar Adrian <oadrian38@gmail.com>
2023-05-03 10:19:10 -0600

    Editados algunos detalles en el archivo app.js

--- src/app.js
+++ src/app.js
@@ -6,8 +6,10 @@ import loginRoutes from './routes/login.routes.js'
 //Instancia App
 const app = express()
 
+//Para dar soporte a los datos que llegan por medio del usuario. Serán usados en request
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
+
 //Archivos estáticos
 app.use(express.static('public'))
 app.use(express.static('bootstrap'))
@@ -16,10 +18,11 @@ app.use(express.static('bootstrap'))
 app.set('views', 'src/public')
 app.set('view engine', 'ejs')
 
-
 //Feed routes
-app.use(signinRoutes)
 app.use(feedRoutes)
+//Signup routes
+app.use(signinRoutes)
+//Login routes
 app.use(loginRoutes)
 
 export default app
